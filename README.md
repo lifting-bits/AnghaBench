a6d93ce5cf60178896a654ad7a583149cce03f44
Artem Dinaburg <artem@trailofbits.com>
2021-04-20 13:25:41 -0700

    Support generation of binaries, armv7, README updates for faster parallel execution

--- README.md
+++ README.md
@@ -3,18 +3,39 @@
 This is a fork of the [AnghaBench Benchmark Suite](https://github.com/brenocfg/AnghaBench).
 This fork includes scripts to build the benchmarks and also includes a means to download pre-compiled benchmarks as well.
 
+
+## Prerequisites
+
+This script only generates compile commands. You need to have the requisite cross-compilers installed. For example, for `armv7` (armv7 hard float, in this case), you probably want:
+
+```sh
+sudo apt install \
+  binutils-arm-linux-gnueabihf\
+  libstdc++6-armhf-cross\
+  gcc-9-arm-linux-gnueabihf
+```
+
+Repeat for `arm64`, x86 (`i686`), etc., as necessary
+
 ## Building
 
 Use the `generate_compile_commands.py` script to generate compile commands, and GNU `parallel` to invoke the build, like so:
 
 ```sh
 scripts/generate_compile_commands.py --clang clang-11 -o compile_commands.txt
-parallel :::: compile_commands.txt
+#Adapted from https://stackoverflow.com/a/11489660
+#this is *considerably* faster than `parallel`
+#but could be improved more since it forks /bin/sh every time
+cores=$(fgrep -c processor /proc/cpuinfo)
+xargs --arg-file=compile_commands.txt \
+      --max-procs=${cores}  \
+      -I{} \
+      /bin/sh -c "{}"
 ```
 
-This will compile the benchmark to bitcode for `x86`, `arm64`, and `x86-64`. Install appropriate cross-compilation tools as needed to get the build to succeed.
+This will compile the benchmark to bitcode for `x86`, `armv7`, `arm64`, and `x86-64`. Install appropriate cross-compilation tools as needed to get the build to succeed.
 
-Compilation takes about 30 minutes per architecture on a Ryzen 9 5950x and the output is around 23 GiB of bitcode.
+Compilation takes about 30 minutes per architecture on a Ryzen 9 5950x.
 
 ## Downloading Pre-Built Bitcode
 
--- scripts/generate_compile_commands.py
+++ scripts/generate_compile_commands.py
@@ -8,87 +8,153 @@ import shutil
 
 MYDIR = path.dirname(path.abspath(__file__))
 ARCHMAP = {
-        'amd64' : ["--target=x86_64-pc-linux-gnu"],
-        'arm64' : ["--target=aarch64-linux-gnu", "-isystem/usr/aarch64-linux-gnu/include"],
-        'x86' :   ["--target=i686-linux-gnu"],
-        }
+    "amd64": ["--target=x86_64-pc-linux-gnu"],
+    "arm64": ["--target=aarch64-linux-gnu", "-isystem/usr/aarch64-linux-gnu/include"],
+    "x86": ["--target=i686-linux-gnu"],
+    "armv7": ["--target=arm-linux-gnueabihf", "-march=armv7a", "-isystem/usr/arm-linux-gnueabihf/include"],
+}
 
 EXTRA_OPTIONS = [
-  '-O1', # do some basic opts
-  '-ggdb', # emit debug info
-  '-pipe', # use less filesystem
-  '-w', # ignore warnings
+    "-O1",  # do some basic opts
+    "-ggdb",  # emit debug info
+    "-pipe",  # use less filesystem
+    "-w",  # ignore warnings
 ]
 
+
 def emit_mk_dir(of, dstdir):
-  of.write(f"mkdir -p {dstdir}\n")
+    of.write(f"mkdir -p {dstdir}\n")
+
+
+def emit_clang_cmdline(of, clang, special_opts, arch, dstfile, srcfile, suffix):
+    cmd_line = [clang]
+    # Options every clang invocation gets
+    cmd_line.extend(EXTRA_OPTIONS)
+    # architecture specific args
+    cmd_line.extend(ARCHMAP[arch])
+    # special arguments for this specific invocation
+    if special_opts:
+        cmd_line.extend(special_opts)
+    # Generate bitcode and not do a full compilation
+    cmd_line.extend(["-o", str(dstfile.with_suffix(suffix)), str(srcfile)])
+
+    # write out the cmdline
+    of.write(" ".join(cmd_line))
+    of.write("\n")
+
 
 def emit_clang_bc_cmdline(of, clang, arch, dstfile, srcfile):
-  cmd_line = [clang]
-  # Options every clang invocation gets
-  cmd_line.extend(EXTRA_OPTIONS)
-  # architecture specific args
-  cmd_line.extend(ARCHMAP[arch])
-  # Generate bitcode and not do a full compilation
-  cmd_line.extend(["-emit-llvm", "-c", "-o", dstfile, srcfile])
+    emit_clang_cmdline(
+        of, clang, ["-emit-llvm", "-c"], arch, dstfile, srcfile, suffix=".bc"
+    )
 
-  # write out the cmdline
-  of.write(" ".join(cmd_line))
-  of.write("\n")
+
+def emit_clang_elf_cmdline(of, clang, arch, dstfile, srcfile):
+    emit_clang_cmdline(of, clang, [], arch, dstfile, srcfile, suffix=".elf")
+
+
+def emit_mkdir_command(
+    of, source_file, source_dir_base, dest_dir_base, previous_dir=None
+):
+    # convert source filename to one in the destination directory tree
+    idx = source_file.parts.index(source_dir_base)
+    new_path = dest_dir_base.joinpath(*source_file.parts[idx + 1 :])
+
+    # emit the mkdir command to create the output directory
+    # skip emitting duplicates, since many files share the same subdirectory
+    new_dir = new_path.parent
+    if new_dir != previous_dir:
+        emit_mk_dir(of, new_dir)
+    return new_dir
+
 
 if __name__ == "__main__":
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--clang",
+        default="clang-11",
+        help="Which clang binary to run, default clang-11",
+    )
+    parser.add_argument(
+        "--source", default=f"{MYDIR}/../source", help="where to look for source files"
+    )
+    parser.add_argument(
+        "--dest", default=f"{MYDIR}/../compiled", help="where to put output"
+    )
+    parser.add_argument(
+        "--emit-bitcode",
+        default=False,
+        action="store_true",
+        help="Emit commands to compile to bitcode",
+    )
+    parser.add_argument(
+        "--emit-binaries",
+        default=False,
+        action="store_true",
+        help="Emit commands to compile binaries",
+    )
+    parser.add_argument(
+        "-o",
+        "--outfile",
+        default="/dev/stdout",
+        help="Output file to write, default to stdout",
+    )
 
-  parser = argparse.ArgumentParser()
-  parser.add_argument('--clang', default="clang-11", help="Which clang binary to run, default clang-11")
-  parser.add_argument('--source', default=f"{MYDIR}/../source", help="where to look for source files")
-  parser.add_argument('--dest', default=f"{MYDIR}/../bitcode", help="where to put bitcode")
-  parser.add_argument('-o', '--outfile', default="/dev/stdout", help="Output file to write, default to stdout")
+    args = parser.parse_args()
 
-  args = parser.parse_args()
+    if not args.emit_bitcode and not args.emit_binaries:
+        sys.stderr.write("Nothing to do.\n")
+        sys.stderr.write("Please specify --emit-bitcode or --emit-binaries\n")
+        sys.exit(1)
 
-  if shutil.which(args.clang) is None:
-    sys.stderr.write(f"Could not find clang command: {args.clang}\n")
-    sys.exit(1)
+    if shutil.which(args.clang) is None:
+        sys.stderr.write(f"Could not find clang command: {args.clang}\n")
+        sys.exit(1)
 
-  source_path = Path(args.source)
-  # find every .c file
-  sources = list(source_path.rglob('*.c'))
+    source_path = Path(args.source)
+    # find every .c file
+    sources = list(source_path.rglob("*.c"))
 
-  # find the last part of the source path, so that we can replicate source tree in destination dir
-  last_source_part = source_path.parts[-1]
+    # find the last part of the source path, so that we can replicate source tree in destination dir
+    last_source_part = source_path.parts[-1]
 
-  if 0 == len(sources):
-    sys.stderr.write(f"Could not find any C source in {args.source}\n")
-    sys.exit(1)
+    if 0 == len(sources):
+        sys.stderr.write(f"Could not find any C source in {args.source}\n")
+        sys.exit(1)
 
-  worklist = []
-  last_dir = ""
+    worklist = []
+    previous_dir = None
 
-  with open(args.outfile, 'w') as of:
-    for (arch, cmdargs) in ARCHMAP.items():
-      destination = Path(f"{args.dest}/{arch}")
+    output_styles = []
+    emit_functions = {}
+    if args.emit_bitcode:
+        output_styles.append("bitcode")
+        emit_functions["bitcode"] = emit_clang_bc_cmdline
+    if args.emit_binaries:
+        output_styles.append("binaries")
+        emit_functions["binaries"] = emit_clang_elf_cmdline
 
-      for source in sources:
+    with open(args.outfile, "w") as of:
+        for (arch, cmdargs) in ARCHMAP.items():
+            for outstyle in output_styles:
+                destination = Path(f"{args.dest}/{outstyle}/{arch}")
+                for source in sources:
+                    previous_dir = emit_mkdir_command(
+                        of, source, last_source_part, destination, previous_dir
+                    )
+                    # save the arch/source/dest pair to avoid recomputing it
+                    worklist.append(
+                        (
+                            arch,
+                            previous_dir.joinpath(source.name),
+                            source,
+                            outstyle,
+                        )
+                    )
 
-        # convert source filename to one in the destination directory tree
-        # and ending in ".bc"
-        source = str(source)
-        f, _ = path.splitext(source)
-        bcfile = Path(f"{f}.bc")
-        idx = bcfile.parts.index(last_source_part)
-        new_path = str(destination.joinpath(*bcfile.parts[idx+1:]))
-
-        # emit the mkdir command to create the output directory
-        # skip emitting duplicates, since many files share the same subdirectory
-        new_dir = path.dirname(new_path)
-        if new_dir != last_dir:
-          last_dir = new_dir
-          emit_mk_dir(of, new_dir)
-
-        # save the arch/source/dest pair to avoid recomputing it
-        worklist.append( (arch, new_path, source,) )
-
-      # emit clang command lines to output bitcode
-      for item in worklist:
-        emit_clang_bc_cmdline(of, args.clang, item[0], item[1], item[2])
+        # emit clang command lines to output bitcode
+        for item in worklist:
+            emit_functions[item[3]](
+                of, args.clang, arch=item[0], dstfile=item[1], srcfile=item[2]
+            )
