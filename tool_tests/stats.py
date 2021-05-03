a5bfe02e9aa87ff228eb82db68c68adcb38e1b2e
Artem Dinaburg <artem@trailofbits.com>
2021-05-03 13:24:59 -0700

    Updated stats

--- tool_tests/stats.py
+++ tool_tests/stats.py
@@ -1,9 +1,11 @@
+from re import I
 import sys
 from threading import Lock
 import json
+from datetime import datetime
+
 
 class Stats:
-
     def __init__(self):
         self.lock = Lock()
         self.stats = {}
@@ -12,10 +14,55 @@ class Stats:
         with open(filepath, "w") as fo:
             json.dump(self.stats, fo, indent=4, sort_keys=True)
 
-    def print_stats(self):
+    def print_fails(self, fail_count=5, output=None):
+        if output is None:
+            output = sys.stderr
+
+        # filter output by
+        out_stats = {
+            k: v
+            for k, v in self.stats.items()
+            if k.startswith("output.") and k != "output.success"
+        }
+        top_items = sorted(out_stats.items(), reverse=True, key=lambda x: len(x[1]))
+
+        for k, v in top_items[:fail_count]:
+            k = k.replace("output.", "")
+            output.write(f"`{k}`: `{len(v)}` failures\n")
+
+    def print_stats(self, output=None):
         # emit start/end time
         # emit %success
-        self.print_json()
+
+        if output is None:
+            output = sys.stderr
+
+        if "start_time" in self.stats and "end_time" in self.stats:
+            start_time = datetime.fromisoformat(self.stats["start_time"])
+            end_time = datetime.fromisoformat(self.stats["end_time"])
+
+            time_diff = end_time - start_time
+            secs = time_diff.total_seconds()
+
+            output.write(f"Run took {time_diff}\n")
+
+            if "program_runs" in self.stats:
+                runs = self.stats["program_runs"]
+                runs_per_sec = runs / secs
+                output.write(f"Speed of {runs_per_sec:.2f} runs/sec\n")
+
+        success_runs = self.stats.get("output.success", [])
+        program_runs = self.stats.get("program_runs", 0)
+
+        success_items = len(success_runs)
+        success_percent = 100.0 * success_items / program_runs
+
+        if program_runs != 0:
+            output.write(f"Success Metrics: [{success_items}/{program_runs}]\n")
+            output.write(f"Success Percentage: [{success_percent:.2f}%]\n")
+
+        if output is not None:
+            output.flush()
 
     def print_json(self):
         self.save_json("/dev/stdout")
